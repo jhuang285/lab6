7dd0aa758f42bc4ce2fb49f6fca63e0de4794cd8
jhuang285 <joh015@ucsd.edu>
2016-02-11 09:17:34 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, callBack);
+	console.log("/project/" +idNumber);
+}
+
+function callBack(result){
+	console.log(result);
+	var id = result.id;
+	var details = '<div class = "detailsImage">' + '<img src = "' + result.image + '" class = "img">'
+	+ '<h4>'+ result.date+ '</h4>' + result.summary + '</div>';
+
+	$("#project" + id + " .details").html(details);
 }
 
 /*
@@ -35,4 +47,14 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", callBack2);
+}
+
+function callBack2(result){
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
